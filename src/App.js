2c6d3eb64ecf2596437178ea38501dd2733cae45
Stephen Csengo <stephencsengo@gmail.com>
2023-06-28 15:52:10 -0400

    Route for CompleteForm added

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ import Header from "./components/Header";
 import HikePage from "./components/HikePage";
 import Navbar from "./components/Navbar";
 import CompletePage from "./components/CompletePage";
+import CompleteForm from "./components/CompleteForm";
 
 function App() {
   return (
     <div className="App">
       <Route path="/completed">
         <CompletePage />
+      </Route>
+      <Route path="/completeform">
+        <CompleteForm />
       </Route>
       <Route exact path="/">
         <Header />
