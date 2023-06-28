e0c4700de14fc8de7735b3db7233d219c6cdfa44
Stephen Csengo <stephencsengo@gmail.com>
2023-06-27 21:21:34 -0400

    Create initial header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header>
+            <h1>Sky's The Limit Hike Tracker</h1>
+            <p>Enter explanation of Sky's The Limit Here</p>
+        </header>
+    )
+}
+
+export default Header;
