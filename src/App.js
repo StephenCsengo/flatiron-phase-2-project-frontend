b8a4a0bbdc3ec4dd339a4103159a2bca74b04322
Stephen Csengo <stephencsengo@gmail.com>
2023-06-27 22:46:48 -0400

    Create basic Card component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import './App.css';
+import "./App.css";
 import Header from "./components/Header";
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,21 @@
 import React from "react";
 
 function Card() {
-    return (
-        <div></div>
-    )
+  return (
+    <div>
+      <img src="https://placehold.co/300x200.png" />
+      <h3>Park Title</h3>
+      <section>
+        <h4>Distance</h4>
+        <h4>Difficulty</h4>
+      </section>
+      <p>
+        <em>Trail Names</em>
+      </p>
+      <p>Description</p>
+      <button>Complete This Hike</button>
+    </div>
+  );
 }
 
 export default Card;
