e04271a11924f1d43f3a871ce895bf690f377e31
Stephen Csengo <stephencsengo@gmail.com>
2023-06-27 21:25:12 -0400

    Create initial navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Navbar() {
+    return(
+        <ul>
+            <li>Home</li>
+            <li>Completed Hikes</li>
+        </ul>
+    )
+}
+
+export default Navbar;
