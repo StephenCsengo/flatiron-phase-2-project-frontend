8faf2dcf390b3684eded309820463db06ec90bef
Stephen Csengo <stephencsengo@gmail.com>
2023-06-28 15:37:07 -0400

    Add Route for completed page

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import "./App.css";
 import Header from "./components/Header";
 import HikePage from "./components/HikePage";
 import Navbar from "./components/Navbar";
+import CompletePage from "./components/CompletePage";
 
 function App() {
   return (
     <div className="App">
-      <Route path="/">
+      <Route path="/completed">
+        <CompletePage />
+      </Route>
+      <Route exact path="/">
         <Header />
         <Navbar />
         <HikePage />
