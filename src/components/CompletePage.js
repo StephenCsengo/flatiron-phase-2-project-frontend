ff130d8769934a04734bace5fde66e43823f6e59
Stephen Csengo <stephencsengo@gmail.com>
2023-06-28 00:02:10 -0400

    Create basic CompletePage component

---
+++ src/components/CompletePage.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+import HikeList from "./HikeList";
+import Container from "@mui/material/Container";
+
+function CompletePage() {
+  const [hikes, setHikes] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/completed")
+      .then((resp) => resp.json())
+      .then((data) => setHikes(data));
+  }, []);
+
+  return (
+    <Container>
+      <HikeList hikes={hikes} />
+    </Container>
+  );
+}
+export default CompletePage;
