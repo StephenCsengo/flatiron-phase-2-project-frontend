bfbf71481bde0a6d5cf1148e5b1c9710a723eb2e
Stephen Csengo <stephencsengo@gmail.com>
2023-06-28 20:34:28 -0400

    Add form elements

--- src/components/CompleteForm.js
+++ src/components/CompleteForm.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 function CompleteForm() {
-  return <p>This is the complete form</p>;
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(e.target.distance.value);
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <select id="hike"></select>
+      <label htmlFor="distance">Miles Hiked</label>
+      <input type="number" id="distance" />
+      <label htmlFor="date">Date Hiked</label>
+      <input type="date" id="date" min="2023-03-07" max="2023-12-03" />
+      <input type="submit" value="Complete Hike" />
+    </form>
+  );
 }
 
 export default CompleteForm;
